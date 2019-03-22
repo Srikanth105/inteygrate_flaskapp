57f11bdc8fb48051a108ec1f83a6b14e31c31a7b
Srikanth105 <34851724+Srikanth105@users.noreply.github.com>
2019-03-22 17:35:50 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -3,7 +3,7 @@ from flask import request
 
 #WA[
 from whatsapp import Client
-expected_token = 'SOME SECRET TOKEN OF YOUR CHOICE'
+expected_token = 'Ticket'
 #]WA
 
 app = Flask(__name__)
@@ -26,7 +26,7 @@ def sendmsg():
     msg = request.args.get('msg')
     token = request.args.get('token')
     if(str(token) == expected_token):
-        client = Client(login='YOUR REGISTERED PHONE NUMBER WITH COUNTRY CODE', password='PASSWORD FROM REGISTRATION')
+        client = Client(login='YOUR REGISTERED PHONE NUMBER WITH COUNTRY CODE', password='cheera234')
         res = client.send_message(to, msg)  
     
     else:
